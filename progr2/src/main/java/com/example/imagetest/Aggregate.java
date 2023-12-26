8852a2db8035956be3937a756476c4eeff0c0295
vanqel <136749556+vanqel@users.noreply.github.com>
2023-12-26 21:00:06 +0300

    Delete progr2 directory

--- progr2/src/main/java/com/example/imagetest/Aggregate.java
+++
@@ -1,5 +0,0 @@
-package com.example.imagetest;
-
-public interface Aggregate {
-    Iterator getIterator();
-}
--- progr2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.imagetest {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.imagetest to javafx.fxml;
-    exports com.example.imagetest;
-}
